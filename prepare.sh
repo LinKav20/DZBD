9361bd306cbbbbfc4db7238f36f7ca8075b1073d
LinKa <maltseva-as@mail.ru>
2022-10-05 23:06:15 +0300

    data add

---
+++ prepare.sh
@@ -0,0 +1,3 @@
+docker volume create --name db_volume_1 --opt type=none --opt device=C:/Users/Linok/Desktop/db/docker/db/postgresql/data1 --opt o=bind
+docker volume create --name db_volume_2 --opt type=none --opt device=C:/Users/Linok/Desktop/db/docker/db/postgresql/data2 --opt o=bind
+docker volume create --name db_volume_3 --opt type=none --opt device=C:/Users/Linok/Desktop/db/docker/db/postgresql/data3 --opt o=bind
